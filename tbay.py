023b3bb92f58956e97a694f10ac04d762309443e
jsudufour <jsudufour@gmail.com>
2017-05-22 19:39:29 -0400

    Fix relationships and add notes on manipulating instances of each class

--- tbay.py
+++ tbay.py
@@ -9,7 +9,17 @@ from sqlalchemy.orm import relationship
 engine = create_engine('postgresql://thinkful:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
-Base = declarative_base()
+Base = declarative_base() #beginning of table creation
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    auctioned_items = relationship("Item", backref="auctioned_by")
+    bids_placed = relationship("Bid", backref="placed_by")
 
 class Item(Base):
     __tablename__ = "items"
@@ -21,10 +31,6 @@ class Item(Base):
     auctioner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     bids_placed = relationship("Bid", backref="placed_on")
 
-    # def __init__(self, name, description):
-    #     self.name = name
-    #     self.description = description
-
 class Bid(Base):
     __tablename__ = "bids"
 
@@ -33,22 +39,31 @@ class Bid(Base):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
 
-    # def __init__(self, price):
-    #     self.price = price
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    auctioned_items = relationship(Item, backref="auctioned_by")
-    bids_placed = relationship(Bid, backref="placed_by")
-
-    # def __init__(self, username, password):
-    #     self.username = username
-    #     self.password = password
+Base.metadata.create_all(engine) #end of table creation
 
 
+dave = User(username="dgrohl", password="everlong")
+alex = User(username="aturner", password="allmyownstunts")
+mike = User(username="mlewis", password="moneyball")
 
-Base.metadata.create_all(engine)
+guitar = Item(name="guitar - limited edition!",description="limited edition guitar recently found in john lennon's hidden cottage in albania")
+pick = Item(name="solid gold guitar pick", description="prototype of a solid gold guitar pick made by up-and-coming music startup muziqcal")
+baseball = Item(name="baseball", description="autographed by billy bean")
+
+session.add_all([dave, alex, mike, guitar, pick, baseball])
+session.commit()
+
+baseball.auctioned_by = mike
+session.add(baseball)
+session.commit()
+baseball.auctioner_id
+mike.id
+mike.auctioned_items
+mike.auctioned_items[0]
+mike.auctioned_items[0].name
+bid = Bid(price=123, placed_on=baseball, placed_by=alex)
+bid.user_id
+session.add(bid)
+session.commit()
+bid.user_id
+bid.item_id
